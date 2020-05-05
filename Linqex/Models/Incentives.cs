55e3a8909f94ad19a5e4bbac963452604da73c85
Ishika1101 <60260727+Ishika1101@users.noreply.github.com>
2020-05-05 14:39:59 +0530

    linq

---
+++ Linqex/Models/Incentives.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Linqex.Models
+{
+    public partial class Incentives
+    {
+        public int EmployeeId { get; set; }
+        public DateTime IncentiveDate { get; set; }
+        public int IncentiveAmount { get; set; }
+
+        public virtual Employee Employee { get; set; }
+    }
+}
---
+++ Linqex/Models/LinqDbContext.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Linqex.Models
+{
+    public partial class LinqDbContext : DbContext
+    {
+        public LinqDbContext()
+        {
+        }
+
+        public LinqDbContext(DbContextOptions<LinqDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Employee> Employee { get; set; }
+        public virtual DbSet<Incentives> Incentives { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=ISHIKAGANGWAL;Database=LinqDb;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.Property(e => e.Department)
+                    .IsRequired()
+                    .HasMaxLength(40)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.FirstName)
+                    .IsRequired()
+                    .HasMaxLength(40)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.JoiningDate).HasColumnType("datetime");
+
+                entity.Property(e => e.LastName)
+                    .IsRequired()
+                    .HasMaxLength(40)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Incentives>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.Property(e => e.IncentiveDate).HasColumnType("datetime");
+
+                entity.HasOne(d => d.Employee)
+                    .WithMany()
+                    .HasForeignKey(d => d.EmployeeId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Incentives_Employee");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
